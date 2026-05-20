a5da8447d1d4debcedebb22c471ea550800b4e88
shreyassrinath94 <shreyassrinath94@gmail.com>
2026-05-20 23:09:58 +0530

    lcd code

--- arduino-project.ino
+++ arduino-project.ino
@@ -1,13 +1,17 @@
+#include <LiquidCrystal.h>
+
+// Initialize LCD (RS, E, D4, D5, D6, D7)
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  // Set up the LCD's number of columns and rows
+  lcd.begin(16, 2);
+
+  // Print a message
+  lcd.setCursor(0, 0);
+  lcd.print("Hello, World!");
 }
 
 void loop() {
-
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-
-
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(600);
+  // Nothing needed here
 }
